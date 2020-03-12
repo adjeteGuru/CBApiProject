f28d4b930d969dcd29c4fc58954a6ff2c5c8f332
adjeteGuru <justinadjev@hotmail.com>
2020-03-12 17:00:48 +0000

    db done and seeded

---
+++ CBApiProject/CBApi.Database/CBApiSeedData.cs
@@ -0,0 +1,30 @@
+﻿using CBApi.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CBApi.Database
+{
+    public static class CBApiSeedData
+    {
+        public static void EnsureSeedData(this CBApiContext db)
+        {
+            if (!db.Users.Any())
+            {
+                var users = new List<User>
+                {
+                    new User
+
+                    {   UserName = "admin",
+                        Password="12345",
+                        CreatedAt=DateTime.Parse("2020-03-04")
+                    }
+
+                };
+                db.Users.AddRange(users);
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- CBApiProject/CBApiProject/Startup.cs
+++ CBApiProject/CBApiProject/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CBApi.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,10 +28,13 @@ namespace CBApiProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<CBApiContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:CBApiDb"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CBApiContext db)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +51,8 @@ namespace CBApiProject
             {
                 endpoints.MapControllers();
             });
+
+            db.EnsureSeedData();
         }
     }
 }
