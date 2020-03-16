e15f3117744d5a71c201a297c9e4dd622ab74960
adjeteGuru <justinadjev@hotmail.com>
2020-03-16 11:50:34 +0000

    refactored

--- CBApiProject/CBApiProject/Queries/UserQuery.cs
+++ CBApiProject/CBApiProject/Queries/UserQuery.cs
@@ -1,23 +1,44 @@
 ﻿using CBApi.DataAccess.Repository.Contracts;
+using CBApi.Database;
+using CBApi.Database.Models;
 using CBApi.Types;
 using GraphQL.Types;
-using System;
+using HotChocolate;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CBApiProject.Queries
 {
-    public class UserQuery : ObjectGraphType
-    {
-        public UserQuery(IUserRepository userRepository)
-        {
-            //pass as generic parameter list of clients 
+    //USE THIS FOR GRAPGQL
 
-            Field<ListGraphType<UserType>>(
-                "users",
-                //this discrib how to resolve the query (e.g: resolve with the Get() from the repository)  
-                resolve: context => userRepository.GetAll());
+    //public class UserQuery : ObjectGraphType
+    //{
+    //    public UserQuery(IUserRepository userRepository)
+    //    {
+    //        //pass as generic parameter list of clients 
+
+    //        Field<ListGraphType<UserType>>(
+    //            "users",
+    //            //this discrib how to resolve the query (e.g: resolve with the Get() from the repository)  
+    //            resolve: context => userRepository.GetAll());
+    //    }
+    //}
+
+    public class UserQuery
+    {
+        public UserQuery()
+        {
+
         }
+
+        public async Task<IReadOnlyList<User>> GetUsers([Service] CBApiContext dbContext)
+        {
+            return await dbContext.Users.ToListAsync();
+
+        }
+
     }
+
 }
